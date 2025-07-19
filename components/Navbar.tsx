46f7e55ed2523d84fa8b00c8ed36508b5a8f12eb
Harikrishnan14 <harikrishnankhd09@gmail.com>
2025-07-19 19:19:19 +0530

    Update: navbar redirection

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,14 @@
+import { useRouter } from 'next/router'
 import React from 'react'
 
 const Navbar = () => {
+    const router = useRouter();
+
     return (
         <div className='bg-white h-[68px] w-full flex items-center px-8 justify-between sticky top-0 z-100'>
             <div className='flex items-center justify-center gap-6'>
-                <h1 className='font-inter font-semibold text-gray-900 text-[24px] leading-[150%] tracking-[0%]'>ticktock</h1>
-                <p className='font-inter font-medium text-[14px] leading-[150%] tracking-[0%] text-center'>Timesheets</p>
+                <h1 className='font-inter font-semibold text-gray-900 text-[24px] leading-[150%] tracking-[0%] cursor-pointer' onClick={() => router.push('/')}>ticktock</h1>
+                <p className='font-inter font-medium text-[14px] leading-[150%] tracking-[0%] text-center cursor-pointer' onClick={() => router.push('/')}>Timesheets</p>
             </div>
             <div className='font-medium text-gray-500 text-base leading-6 tracking-normal'>
                 <button>John Doe</button>
